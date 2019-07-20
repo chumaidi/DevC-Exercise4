b7d73385c4a08e87a95f9449a18672256493b29a
chumaidi <43468985+chumaidi@users.noreply.github.com>
2019-07-20 16:07:05 +0700

    exercise 3 and 4

---
+++ index.js
@@ -0,0 +1,33 @@
+<h1 class=text-center>Daftar Planet</h1>
+<input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for planet names...">
+<table class=table text-center>
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Planet Name</th>
+            <th scope="col">Rotation Period</th>
+            <th scope="col">Orbital Period</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th scope="row">1</th>
+            <td>Alderaan</td>
+            <td>24</td>
+            <td>364</td>
+        </tr>
+        <tr>
+            <th scope="row">2</th>
+            <td>Yavin IV</td>
+            <td>24</td>
+            <td>4818</td>
+        </tr>
+        <tr>
+            <th scope="row">3</th>
+            <td>Hoth</td>
+            <td>23</td>
+            <td>549</td>
+        </tr>
+    </tbody>
+</table> -->
+</div>
